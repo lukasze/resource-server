da1b383037cf731f09031c28a2548bc1231d9b64
uki <wyspianski.lukasz@gmail.com>
2021-12-17 10:56:02 +0100

    Lombbok, konfiguracja h2, model, data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO workout
+(id, user, start, end, difficulty) VALUES
+(1, 'lukasz', '2021-12-16 15:05:05', '22021-12-16 16:10:07', '3');
+
+INSERT INTO workout
+(id, user, start, end, difficulty) VALUES
+(2, 'michal', '2021-12-16 15:05:10', '2021-12-16 16:10:20', '3');
+
+INSERT INTO workout
+(id, user, start, end, difficulty) VALUES
+(3, 'piotr', '2021-12-17 12:00:10', '2021-12-17 13:01:10', '4');
+
+INSERT INTO workout
+(id, user, start, end, difficulty) VALUES
+(4, 'konrad', '2021-12-18 12:00:05', '2021-12-18 12:00:11', '4');
